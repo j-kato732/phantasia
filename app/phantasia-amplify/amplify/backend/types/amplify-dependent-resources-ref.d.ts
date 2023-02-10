445149431a4e511934610e21facc588971d312fc
j-kato732 <jinini1212@gmail.com>
2023-02-10 20:43:41 +0900

    lambda関数の追加

--- app/phantasia-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/phantasia-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,11 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "phantasiaamplify785c69d5": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}

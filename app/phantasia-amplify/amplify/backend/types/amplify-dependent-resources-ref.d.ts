58a3c40b447176885cd732d77e8d6405952985ae
j-kato732 <jinini1212@gmail.com>
2023-02-14 14:03:45 +0900

    delete function

--- app/phantasia-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/phantasia-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "function": {
-    "phantasiaamplify785c69d5": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
